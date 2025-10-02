e7d6f990381120a5125cf0d532c6b0ce7486397a
r-kojima <kojimar24@gmail.com>
2025-10-02 09:35:31 +0900

    feat: render markdown

---
+++ app/components/markdown-renderer.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+
+interface MarkdownRendererProps {
+	content: string;
+}
+
+export function MarkdownRenderer({ content }: MarkdownRendererProps) {
+	return (
+		<div className="prose prose-slate dark:prose-invert max-w-none">
+			<ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
+				{content}
+			</ReactMarkdown>
+		</div>
+	);
+}
